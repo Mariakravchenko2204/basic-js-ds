c59007ca1b399a0e7f268dd7b02816d9390e3ff6
Mariia Kravchenko <mariia.kravchenko2204@gmail.com>
2025-01-02 20:39:43 -0500

    init: submit stack and remove from list tasks

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let current = l;
+  let prev = null;
+  while (current) {
+    if (current.value === k) {
+      if (prev === null) {
+        l = new ListNode(current.next.value);
+        l.next = current.next.next;
+        current = l;
+      } else {
+        if (current.next) {
+          prev.next = current.next;
+          current = current.next;
+        } else {
+          prev.next = null;
+          current = null;
+        }
+      }
+    } else {
+      prev = current;
+      current = current.next;
+    }
+  }
+  return l;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
